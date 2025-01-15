db959dea13449997c5a67b3b4d956541c3baaa58
Twoje Imię i Nazwisko <twojemail@example.com>
2025-01-15 22:37:25 +0100

    First commit

--- src/test/java/TestCases/RegistrationFlowTest.java
+++ src/test/java/TestCases/RegistrationFlowTest.java
@@ -1,10 +1,10 @@
 package TestCases;
 
 import Base.Hook;
+import Pages.CourseTypePage;
 import Pages.StartPage;
 import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -13,34 +13,25 @@ import java.util.Arrays;
 import java.util.List;
 
 import static Objects.StartPageObjects.*;
-
+import static Objects.StartPageObjects.advertisementAgreedInput;
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class RegistrationFormTest extends Hook {
-
+public class RegistrationFlowTest extends Hook {
 
     @Steps
     StartPage startPage;
+
+    @Steps
+    CourseTypePage courseTypePage;
+
     List<String> listOfWebElements = Arrays.asList(parentNameInput, emailAddressInput, phoneNumberInput, birthYearInput, statuteAgreedInput, advertisementAgreedInput);
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/UserData.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/Data.csv", numLinesToSkip = 1)
     void testCheckingRegistration(String parentName, String emailAddress, String mobilePhone, String yearOfBirth, String statuteAgreedInput, String advertisementAgreedInput) throws InterruptedException {
         List<String> listOfInputs = Arrays.asList(parentName, emailAddress, mobilePhone, yearOfBirth, statuteAgreedInput, advertisementAgreedInput);
-
         startPage.fillOutTheForm(parentName, emailAddress, mobilePhone, yearOfBirth, statuteAgreedInput, advertisementAgreedInput);
-        startPage.checkErrorPopup(listOfInputs);
-        for (int i = 0; i < listOfWebElements.size(); i++) {
-            boolean isCorrect = startPage.verifyCommunicate(listOfWebElements.get(i), listOfInputs.get(i));
-            if (!isCorrect) {
-                System.out.println("The message for the element " + listOfWebElements.get(i) + " is not displayed correctly");
-            }
-            Assertions.assertTrue(isCorrect);
-        }
-        Assertions.assertTrue(startPage.validateFormAndVisibility(listOfInputs));
-
-
+        courseTypePage.clickOnProgramowanie("Online", "Roczne kursy z programowania");
     }
-
 
 }
